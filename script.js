2cecea0d5e686995dabd31f7b2bf8555e371821a
iblamealex1-web <iblamealex1@gmail.com>
2026-01-31 13:07:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,5 +8,23 @@ function goNext() {
     document.getElementById("page" + current).classList.add("active");
   }
 }
+const photos = [
+  "images/photo1.jpg",
+  "images/photo2.jpg",
+  "images/photo3.jpg",
+  "images/photo4.jpg",
+  "images/photo5.jpg"
+];
 
+let photoIndex = 0;
+
+function nextPhoto() {
+  photoIndex = (photoIndex + 1) % photos.length;
+  document.getElementById("albumImg").src = photos[photoIndex];
+}
+
+function prevPhoto() {
+  photoIndex = (photoIndex - 1 + photos.length) % photos.length;
+  document.getElementById("albumImg").src = photos[photoIndex];
+}
   
